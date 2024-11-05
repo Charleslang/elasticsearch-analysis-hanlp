005576c6439ede4b2146a87fe59e9e44df037681
qingjing <daijunfeng.me@qq.com>
2024-11-05 17:18:58 +0800

    fix: Cannot read field featureMap because model is null

--- src/main/java/com/hankcs/lucene/HanLPNLPAnalyzer.java
+++ src/main/java/com/hankcs/lucene/HanLPNLPAnalyzer.java
@@ -1,11 +1,14 @@
 package com.hankcs.lucene;
 
 import com.hankcs.cfg.Configuration;
+import com.hankcs.hanlp.HanLP;
 import com.hankcs.hanlp.model.perceptron.PerceptronLexicalAnalyzer;
 import com.hankcs.hanlp.seg.Segment;
 import com.hankcs.model.PerceptronCWSInstance;
 import com.hankcs.model.PerceptronNERInstance;
 import com.hankcs.model.PerceptronPOSInstance;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.lucene.analysis.Analyzer;
 
 import java.security.AccessController;
@@ -18,6 +21,8 @@ import java.security.PrivilegedAction;
  * Create: 2018-12-14 15:10
  */
 public class HanLPNLPAnalyzer extends Analyzer {
+
+    private static final Logger logger = LogManager.getLogger(HanLPNLPAnalyzer.class);
 
     /**
      * 分词配置
@@ -33,12 +38,17 @@ public class HanLPNLPAnalyzer extends Analyzer {
     protected Analyzer.TokenStreamComponents createComponents(String fieldName) {
         return new Analyzer.TokenStreamComponents(
                 TokenizerBuilder.tokenizer(
-                        AccessController.doPrivileged((PrivilegedAction<Segment>) () ->
-                                new PerceptronLexicalAnalyzer(
+                        AccessController.doPrivileged((PrivilegedAction<Segment>) () -> {
+                            try {
+                                return new PerceptronLexicalAnalyzer(
                                         PerceptronCWSInstance.getInstance().getLinearModel(),
                                         PerceptronPOSInstance.getInstance().getLinearModel(),
                                         PerceptronNERInstance.getInstance().getLinearModel()
-                                )),
-                        configuration));
+                                );
+                            } catch (Exception e) {
+                                logger.warn("can not use nlp analyzer, provider default");
+                                return HanLP.newSegment();
+                            }
+                        }), configuration));
     }
 }
